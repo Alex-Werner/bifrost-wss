40222b97ce0b278b167a1042e5663e549547085a
Alex Werner <obusco@gmail.com>
2023-12-23 04:22:14 +0100

    docs: added do not exist log

--- src/WSSManager/methods/addPeerToRoom.js
+++ src/WSSManager/methods/addPeerToRoom.js
@@ -1,5 +1,6 @@
 export default function addPeerToRoom(peer, roomName) {
     if(!this.rooms.has(roomName)) {
+        this.logger.method('addPeerToRoom').error(`Room ${roomName} does not exist`)
         return new Error(`Room ${roomName} does not exist`)
     }
     this.rooms.get(roomName).set(peer.id, peer);
