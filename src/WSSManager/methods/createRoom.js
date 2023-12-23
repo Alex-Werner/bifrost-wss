45b54d500414fbd71f92fd9ea5d47656989772dd
Alex Werner <obusco@gmail.com>
2023-12-23 04:21:02 +0100

    feat!: add force create room

--- src/WSSManager/methods/createRoom.js
+++ src/WSSManager/methods/createRoom.js
@@ -1,3 +1,8 @@
-export default function createRoom(roomName) {
+export default function createRoom(roomName, force = false) {
+    if(this.rooms.has(roomName) && force) {
+        this.logger.method('createRoom').error(`Room ${roomName} already exists`)
+        return new Error(`Room ${roomName} already exists`)
+    }
     this.rooms.set(roomName, new Map());
+    this.logger.method('createRoom').log(`Room ${roomName} created`)
 }
