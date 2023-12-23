71e6d8ba5c2938d37d7ff463c55186a723531f44
Alex Werner <obusco@gmail.com>
2023-12-23 04:21:38 +0100

    feat!: doNotSelfBroadcast by default
    if sender is in the room, do not send to him

--- src/WSSManager/methods/broadcastRoom.js
+++ src/WSSManager/methods/broadcastRoom.js
@@ -1,15 +1,16 @@
-function broadcastRoom(room, message) {
-    this.logger.method('broadcastRoom').log(`-> Broadcasting to room ${room}`);
+function broadcastRoom(room, message, sender = null, broadcastToSelf = false) {
+    const logger = this.logger.method('broadcastRoom');
+    logger.log(`-> Broadcasting to room ${room} - ${message}`);
     const peers = this.rooms.get(room);
-
     if (!peers) {
         return;
     }
-
     peers.forEach((peer) => {
+        if (sender && !broadcastToSelf && sender.id === peer.id) {
+            return;
+        }
         peer.send(message);
     });
-
-    this.logger.method('broadcastRoom').trace(`<- Broadcasted to ${peers.size} peers in room ${room}`);
+    logger.log(`<- Broadcasted to ${peers.size} peers in room ${room}`);
 }
 export default broadcastRoom;
